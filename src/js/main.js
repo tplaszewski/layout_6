8380e5448684bf73331c99a82e72f4df5b6666a6
tplaszewski <t.plaszewski@gmail.com>
2015-10-23 09:14:16 +0200

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -52,25 +52,117 @@ jQuery(document).ready(function( $ ) {
 window.onload = function () {
             var styles = [
     {
-        "featureType": "all",
+        "featureType": "water",
         "elementType": "all",
         "stylers": [
             {
-                "invert_lightness": true
+                "hue": "#76aee3"
             },
             {
-                "saturation": 10
+                "saturation": 38
             },
             {
-                "lightness": 30
+                "lightness": -11
             },
             {
-                "gamma": 0.5
-            },
-            {
-                "hue": "#435158"
+                "visibility": "on"
             }
         ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#8dc749"
+            },
+            {
+                "saturation": -47
+            },
+            {
+                "lightness": -17
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#c6e3a4"
+            },
+            {
+                "saturation": 17
+            },
+            {
+                "lightness": -2
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#cccccc"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 13
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#5f5855"
+            },
+            {
+                "saturation": 6
+            },
+            {
+                "lightness": -31
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": []
     }
 ];
 
